af796d3848113a58d259501ea7d2901a23d07d31
krishna.achanta <achanta3215@gmail.com>
2020-08-05 20:21:39 +0530

    Add error handling for main script

--- app.js
+++ app.js
@@ -45,47 +45,51 @@ const preparePageForTests = async (page) => {
     args: ['--no-sandbox'],
     // devtools: true,
   });
-  const page = await browser.newPage();
-  await preparePageForTests(page);
-  await page.goto('https://www.mohfw.gov.in/');
-  await page.waitForSelector('#state-data tbody tr');
-  const statesData = await page.evaluate(async () => {
-    const statesQuery = document.querySelectorAll('#state-data tbody tr');
-    console.log(Array.from(statesQuery));
-    return Array.from(statesQuery)
-      .map((states) => ({
-        state: states.children[1] && states.children[1].innerHTML,
-        active: states.children[2] && states.children[2].innerHTML,
-        discharged: states.children[3] && states.children[3].innerHTML,
-        deceased: states.children[4] && states.children[4].innerHTML,
-        total: states.children[5] && states.children[5].innerHTML,
-      }))
-      .filter((data) => Boolean(data.state));
-  });
-  const today = new Date();
-  const statsObjTodayKey = getStatsKeyFromDate(today);
-  if (!isStatsLoaded) {
-    console.log('Unable to load stats file');
-    process.exit(0);
+  try {
+    const page = await browser.newPage();
+    await preparePageForTests(page);
+    await page.goto('https://www.mohfw.gov.in/');
+    await page.waitForSelector('#state-data tbody tr');
+    const statesData = await page.evaluate(async () => {
+      const statesQuery = document.querySelectorAll('#state-data tbody tr');
+      console.log(Array.from(statesQuery));
+      return Array.from(statesQuery)
+        .map((states) => ({
+          state: states.children[1] && states.children[1].innerHTML,
+          active: states.children[2] && states.children[2].innerHTML,
+          discharged: states.children[3] && states.children[3].innerHTML,
+          deceased: states.children[4] && states.children[4].innerHTML,
+          total: states.children[5] && states.children[5].innerHTML,
+        }))
+        .filter((data) => Boolean(data.state));
+    });
+    const today = new Date();
+    const statsObjTodayKey = getStatsKeyFromDate(today);
+    if (!isStatsLoaded) {
+      console.log('Unable to load stats file');
+      process.exit(0);
+    }
+    if (statsObjData[statsObjTodayKey]) {
+      console.log(`Data for today ${statsObjTodayKey} already exists`);
+      console.log('Exiting');
+      process.exit(0);
+    }
+    const transformedStatsObj = transformStatesData(
+      statesData,
+      today,
+      statsObjData,
+    );
+    fs.writeFile(covidStatsFile, JSON.stringify(transformedStatsObj), () => {});
+    const newStatsObj = transformedStatsObj[statsObjTodayKey];
+    fs.writeFile(
+      `${covidStatsDir}/dateWiseStats_${statsObjTodayKey}.json`,
+      JSON.stringify(newStatsObj),
+      () => {},
+    );
+  } catch(e){}
+  finally {
+    browser.close();
   }
-  if (statsObjData[statsObjTodayKey]) {
-    console.log(`Data for today ${statsObjTodayKey} already exists`);
-    console.log('Exiting');
-    process.exit(0);
-  }
-  const transformedStatsObj = transformStatesData(
-    statesData,
-    today,
-    statsObjData,
-  );
-  fs.writeFile(covidStatsFile, JSON.stringify(transformedStatsObj), () => {});
-  const newStatsObj = transformedStatsObj[statsObjTodayKey];
-  fs.writeFile(
-    `${covidStatsDir}/dateWiseStats_${statsObjTodayKey}.json`,
-    JSON.stringify(newStatsObj),
-    () => {},
-  );
-  browser.close();
 })();
 
 module.exports = {
